30864e3a7e89f12539dc7d97d43a701227402e0c
Haider-x31 <ggvvffhhbbvv@gmail.com>
2026-03-30 02:44:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import os
 
-# الصحيح: تجيب التوكن من Variables
-BOT_TOKEN = os.getenv("8686945908:AAH79liYdVN2fj0fj7LQMKXA3R4xZhFQCRg")
+TOKEN = "8686945908:AAH79liYdVN2fj0fj7LQMKXA3R4xZhFQCRg"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🔥 البوت شغال بنجاح")
+    await update.message.reply_text("البوت شغال ✅")
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
-print("Bot is running...")
-app.run_polling()
+if __name__ == "__main__":
+    main()
